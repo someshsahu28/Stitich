6c8b940147662ee641ae7625f8e7509faca02ef7
someshsahu28 <someshsahu.sagar@gmail.com>
2026-04-18 18:02:54 +0530

    Implement the products

--- Frontend/src/app/app.routes.jsx
+++ Frontend/src/app/app.routes.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import Register from "../features/auth/pages/Register";
 import Login from "../features/auth/pages/Login";
+import CreateProduct from "../features/products/pages/CreateProduct";
+import Dashboard from "../features/products/pages/Dashboard";
+import Protected from "../features/auth/components/Protected";
+import Home from "../features/products/pages/Home";
+import ProductDetail from "../features/products/pages/ProductDetail";
+import SellerProductDetails from "../features/products/pages/SellerProductDetails";
 
 export const routes = createBrowserRouter([
     {
         path: "/",
-        element: <h1>Hello world</h1>,
+        element: <Home />,
     },
     {
         path: "/register",
@@ -14,5 +20,34 @@ export const routes = createBrowserRouter([
     {
         path: "/login",
         element: <Login />,
+    },
+    {
+        path: "/product/:productId",
+        element: <ProductDetail />
+    },
+    {
+        path: "/seller",
+        children: [
+            {
+                path: "/seller/create-product",
+
+                element: <Protected role="seller" >
+                    <CreateProduct />
+                </Protected>
+            },
+            {
+                path: "/seller/dashboard",
+                element: <Protected role="seller" >
+                    <Dashboard />
+                </Protected>
+            },
+            {
+                path: "/seller/product/:productId",
+                element: <Protected role="seller" >
+                    <SellerProductDetails />
+                </Protected>
+            }
+        ]
     }
+
 ])
---
+++ Frontend/src/features/products/services/product.api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const productApiInstance = axios.create({
+    baseURL: "/api/products",
+    withCredentials: true,
+})
+
+export async function createProduct(formData) {
+    const response = await productApiInstance.post("/", formData)
+
+    return response.data
+}
+
+export async function getSellerProduct() {
+    const response = await productApiInstance.get("/seller")
+    return response.data
+}
+
+export async function getAllProducts() {
+    const response = await productApiInstance.get("/")
+    return response.data
+}
+
+export async function getProductById(productId) {
+    const response = await productApiInstance.get(`/detail/${productId}`)
+    return response.data
+}
